73a0f1ddfb456ced9bcb63f2fed7ecf315860ba6
Steven van Tetering <steven@tikveel.nl>
2021-02-12 12:10:54 +0100

    Fix header size in write_bmp_header()

--- fbscreenshot.c
+++ fbscreenshot.c
@@ -10,17 +10,17 @@
 #include <sys/ioctl.h>
 
 void write_bmp_header(char* buffer, int width, int height, int bytes_per_pixel, size_t image_size) {
-    size_t file_size = image_size + 52;
+    size_t file_size = image_size + 54;
 
     // Set all bytes in the header to 0
-    memset(buffer, 0, 52);
+    memset(buffer, 0, 54);
 
     // Magic number: BM
     buffer[0x00] = 0x42;
     buffer[0x01] = 0x4D;
 
     // File size
-    char* file_size_header = &buffer[2];
+    char* file_size_header = &buffer[0x02];
     memcpy(file_size_header, (uint32_t*)&file_size, sizeof(uint32_t));
 
     // Pixel data starting address
@@ -126,6 +126,7 @@ int main(int argc, char** argv) {
     
     size_t image_size = capture_width * capture_height * bytes_per_pixel + padding_bytes * capture_height;
     size_t file_size = image_size + 54;
+    
     char data[file_size];
     write_bmp_header(&data[0], capture_width, -capture_height, bytes_per_pixel, image_size);
 
